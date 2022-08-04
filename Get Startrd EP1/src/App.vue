45a6336298f5a97a6122a205fd1c42d1a3db0917
hungcurry <ooopp42@gmail.com>
2022-08-04 18:30:18 +0800

    Get Startrd EP1

---
+++ Get Startrd EP1/src/App.vue
@@ -0,0 +1,44 @@
+<!-- npm方式-Options API -->
+<!-- <script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
+  mounted() {
+    console.log(`The initial count is ${this.count}.`)
+  }
+}
+</script>
+
+<template>
+  <h1>Vue : {{ count }}</h1>
+  <button @click="increment">Count is: {{ count }}</button>
+</template> -->
+
+
+
+<!-- npm方式-Composition API -->
+<script setup>
+import { ref, onMounted } from 'vue'
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`)
+})
+</script>
+
+<template>
+  <h1>Vue : {{ count }}</h1>
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
